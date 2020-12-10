8e970bd83cf54bd330de49448d58d4c0fb30408f
Ryan Habis <30758550+ryanhabis@users.noreply.github.com>
2020-12-10 15:28:54 +0000

    Add files via upload

---
+++ DicePairs/DicePairs/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace DicePairs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.BtnExit = new System.Windows.Forms.Button();
+            this.lstBox = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.BtnArray = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPlay.Location = new System.Drawing.Point(81, 285);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(87, 77);
+            this.btnPlay.TabIndex = 0;
+            this.btnPlay.Text = "Play Dice Game";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(764, 12);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(108, 54);
+            this.btnClear.TabIndex = 1;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // BtnExit
+            // 
+            this.BtnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnExit.Location = new System.Drawing.Point(764, 83);
+            this.BtnExit.Name = "BtnExit";
+            this.BtnExit.Size = new System.Drawing.Size(108, 54);
+            this.BtnExit.TabIndex = 2;
+            this.BtnExit.Text = "Exit";
+            this.BtnExit.UseVisualStyleBackColor = true;
+            this.BtnExit.Click += new System.EventHandler(this.BtnExit_Click);
+            // 
+            // lstBox
+            // 
+            this.lstBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstBox.FormattingEnabled = true;
+            this.lstBox.ItemHeight = 25;
+            this.lstBox.Location = new System.Drawing.Point(94, 28);
+            this.lstBox.Name = "lstBox";
+            this.lstBox.Size = new System.Drawing.Size(378, 229);
+            this.lstBox.TabIndex = 3;
+            this.lstBox.SelectedIndexChanged += new System.EventHandler(this.lstBox_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(185, 286);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(74, 77);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Throw One Dice";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(281, 286);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(79, 77);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Coin Toss";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // BtnArray
+            // 
+            this.BtnArray.Location = new System.Drawing.Point(391, 292);
+            this.BtnArray.Name = "BtnArray";
+            this.BtnArray.Size = new System.Drawing.Size(90, 71);
+            this.BtnArray.TabIndex = 6;
+            this.BtnArray.Text = "Array Dice";
+            this.BtnArray.UseVisualStyleBackColor = true;
+            this.BtnArray.Click += new System.EventHandler(this.BtnArray_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 482);
+            this.Controls.Add(this.BtnArray);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lstBox);
+            this.Controls.Add(this.BtnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnPlay);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button BtnExit;
+        private System.Windows.Forms.ListBox lstBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button BtnArray;
+    }
+}
+
---
+++ DicePairs/DicePairs/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DicePairs
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            lstBox.Items.Clear();
+            Random randomnum = new Random();
+            int num1 = 0, num2 = 0, matches = 0;
+            for (int i = 0; i < 6; i++)
+            {
+                num1 = randomnum.Next(1, 7);
+                num2 = randomnum.Next(1, 7);
+
+                lstBox.Items.Add(num1 + "\t" + num2);
+                   if (num1 == num2)
+                    matches = matches + 1;
+            }
+
+            lstBox.Items.Add("No of matches: " + matches);
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            lstBox.Items.Clear();
+
+        }
+
+        private void BtnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lstBox.Items.Clear();
+
+            Random randomnum = new Random();
+            int dice = 0, x = 0, num1 = 0, num2=0, num3 = 0, num4 = 0, num5 = 0, num6 = 0;
+
+            while (x < 1000)
+            {
+                dice = randomnum.Next(1, 7);
+              
+                if (dice == 1)
+                {
+                    num1 = num1 + 1;
+                }
+                else if (dice == 2)
+                {
+                    num2 = num2 + 1;
+                }
+                else if (dice == 3)
+                {
+                    num3 = num3 + 1;
+                }
+                else if (dice == 4)
+                {
+                    num4 = num4+ 1;
+                }
+                else if (dice == 5)
+                {
+                    num5 = num5+1;
+                }
+                else if (dice == 6)
+                {
+                    num6 = num6+1;
+                }
+                else
+                { 
+                    MessageBox.Show("No Dice");
+                }
+
+                x = x + 1;
+
+            }
+
+            lstBox.Items.Add("Number 1 " + "\t" + num1.ToString());
+            lstBox.Items.Add("Number 2 " + "\t" + num2.ToString());
+            lstBox.Items.Add("Number 3 " + "\t"+ num3.ToString());
+            lstBox.Items.Add("Number 4 " + "\t" + num4.ToString());
+            lstBox.Items.Add("Number 5 " + "\t" + num5.ToString());
+            lstBox.Items.Add("Number 6 " + "\t" + num6.ToString());
+
+        }
+
+        private void BtnArray_Click(object sender, EventArgs e)
+        {
+
+            int[] dice = new int[7];
+            int number = 0;
+            Random rand = new Random();
+            lstBox.Items.Clear();
+            for (int i = 0; i<10; i++)
+            {
+                number = rand.Next(1, 7);
+                dice[number] = dice[number] + 1;
+            }
+
+            for (int x = 1; x < dice.Length; x++)
+            {
+                lstBox.Items.Add("Dice number = " + x + " landed " + dice[x].ToString() + " times");
+            }
+            lstBox.Items.Add("Sum of the elements are = " + dice.Sum());
+        }
+
+        private void lstBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
